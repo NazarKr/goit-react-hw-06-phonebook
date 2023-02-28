c3aa70838f10376f0731289ab4c2bb66bfb2b638
NazarKr <nazarok1990@gmail.com>
2023-02-28 11:37:26 +0200

    add persist

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,21 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { FilterForm } from './FilterForm/FilterForm';
 import { ContactList } from './ContactList/ContactList';
 import { getContacts } from "redux/selectors";
+import { PersistGate } from 'redux-persist/integration/react';
+import {persistor} from "redux/store"
 
 export const App = () => {
   const contacts = useSelector(getContacts);
   return (
+
+    <PersistGate loading={null} persistor={persistor}>
     <Layout>
       <AppBar />
       <ContactForm />
       <FilterForm />
       {contacts.length > 0 &&  <ContactList/>}
     </Layout>
+    </PersistGate>
+
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactSlice';
 import { filterReducer } from './filtersSlice';
+import storage from 'redux-persist/lib/storage'; 
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+  persistReducer,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
